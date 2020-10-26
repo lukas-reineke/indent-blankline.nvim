3cf3347b5e8a071b382208ebba1bc11146bc0b89
Lukas Reineke <lukas.reineke@protonmail.com>
2020-10-26 20:32:39 +0900

    formatted with luafmt

--- lua/indent_blankline.lua
+++ lua/indent_blankline.lua
@@ -1,34 +1,32 @@
-local co = coroutine
-
 local M = {}
 
-vim.g.indent_blankline_namespace = vim.api.nvim_create_namespace('indent_blankline')
+vim.g.indent_blankline_namespace = vim.api.nvim_create_namespace("indent_blankline")
 
 local function error_handler(err)
     if vim.g.indent_blankline_debug then
-        vim.api.nvim_command('echohl Error')
+        vim.api.nvim_command("echohl Error")
         vim.api.nvim_command('echom "' .. err .. '"')
-        vim.api.nvim_command('echohl None')
+        vim.api.nvim_command("echohl None")
     end
 end
 
 local set_indent_blankline_enabled = function()
     vim.b.indent_blankline_enabled = true
 
-    for i=1, #vim.g.indent_blankline_filetype_exclude do
+    for i = 1, #vim.g.indent_blankline_filetype_exclude do
         if vim.g.indent_blankline_filetype_exclude[i] == vim.bo.filetype then
             vim.b.indent_blankline_enabled = false
         end
     end
 
-    for i=1, #vim.g.indent_blankline_buftype_exclude do
+    for i = 1, #vim.g.indent_blankline_buftype_exclude do
         if vim.g.indent_blankline_buftype_exclude[i] == vim.bo.buftype then
             vim.b.indent_blankline_enabled = false
         end
     end
 
     if #vim.g.indent_blankline_filetype > 0 then
-        for i=1, #vim.g.indent_blankline_filetype do
+        for i = 1, #vim.g.indent_blankline_filetype do
             if vim.g.indent_blankline_filetype[i] == vim.bo.filetype then
                 vim.b.indent_blankline_enabled = true
             end
@@ -36,9 +34,9 @@ local set_indent_blankline_enabled = function()
         vim.b.indent_blankline_enabled = false
     end
 
-    local bufname = vim.fn['bufname']('')
-    for i=1, #vim.g.indent_blankline_bufname_exclude do
-        if vim.fn['matchstr'](bufname, vim.g.indent_blankline_bufname_exclude[i]) == bufname then
+    local bufname = vim.fn["bufname"]("")
+    for i = 1, #vim.g.indent_blankline_bufname_exclude do
+        if vim.fn["matchstr"](bufname, vim.g.indent_blankline_bufname_exclude[i]) == bufname then
             vim.b.indent_blankline_enabled = false
         end
     end
@@ -49,14 +47,12 @@ local set_indent_blankline_enabled = function()
 end
 
 local clear_line_indent = function(buf, lnum)
-    xpcall(function()
-        vim.api.nvim_buf_clear_namespace(
-            buf,
-            vim.g.indent_blankline_namespace,
-            lnum - 1,
-            lnum
-        )
-    end, error_handler)
+    xpcall(
+        function()
+            vim.api.nvim_buf_clear_namespace(buf, vim.g.indent_blankline_namespace, lnum - 1, lnum)
+        end,
+        error_handler
+    )
 end
 
 local refresh = function()
@@ -88,81 +84,87 @@ local refresh = function()
 
     for i = 1, #lines do
         local async
-        async = vim.loop.new_async(function()
-            if lines[i]:len() > 0 then
-                vim.schedule_wrap(
-                    function()
-                        clear_line_indent(buf, i)
-                    end
-                )()
-                return async:close()
-            end
-
-            local indent
-            if empty_line_counter > 0 then
-                empty_line_counter = empty_line_counter - 1
-                indent = next_indent
-            else
-                if i == #lines then
-                    indent = 0
-                else
-                    local j = i + 1
-                    while(j < #lines and lines[j]:len() == 0) do
-                        j = j + 1
-                        empty_line_counter = empty_line_counter + 1
-                    end
-                    local _
-                    _, indent = lines[j]:find('^%s+')
-                end
-                next_indent = indent
-            end
-
-            if not indent then
-                vim.schedule_wrap(
-                    function()
-                        clear_line_indent(buf, i)
-                    end
-                )()
-                return async:close()
-            end
-
-            if (expandtab) then
-                indent = indent / space
-            end
-
-            if extra_indent_level then
-                indent = indent + extra_indent_level
-            end
-
-            local v_text = {}
-            for i = 1, math.min(math.max(indent, 0), max_indent_level) do
-                local c
-
-                if #char_list > 0 then
-                    c = char_list[((i - 1) % #char_list) + 1]
-                else
-                    c = char
+        async =
+            vim.loop.new_async(
+            function()
+                if lines[i]:len() > 0 then
+                    vim.schedule_wrap(
+                        function()
+                            clear_line_indent(buf, i)
+                        end
+                    )()
+                    return async:close()
                 end
 
-                v_text[i * 2 - 1] = {space_char:rep(space - 1), 'Conceal'}
-                v_text[i * 2] = {c, 'Whitespace'}
-            end
+                local indent
+                if empty_line_counter > 0 then
+                    empty_line_counter = empty_line_counter - 1
+                    indent = next_indent
+                else
+                    if i == #lines then
+                        indent = 0
+                    else
+                        local j = i + 1
+                        while (j < #lines and lines[j]:len() == 0) do
+                            j = j + 1
+                            empty_line_counter = empty_line_counter + 1
+                        end
+                        local _
+                        _, indent = lines[j]:find("^%s+")
+                    end
+                    next_indent = indent
+                end
 
-            vim.schedule_wrap(
-                function()
-                    xpcall(function()
-                        vim.api.nvim_buf_set_virtual_text(
-                            buf,
-                            vim.g.indent_blankline_namespace,
-                            i - 1,
-                            v_text,
-                            vim.empty_dict()
+                if not indent then
+                    vim.schedule_wrap(
+                        function()
+                            clear_line_indent(buf, i)
+                        end
+                    )()
+                    return async:close()
+                end
+
+                if (expandtab) then
+                    indent = indent / space
+                end
+
+                if extra_indent_level then
+                    indent = indent + extra_indent_level
+                end
+
+                local v_text = {}
+                for i = 1, math.min(math.max(indent, 0), max_indent_level) do
+                    local c
+
+                    if #char_list > 0 then
+                        c = char_list[((i - 1) % #char_list) + 1]
+                    else
+                        c = char
+                    end
+
+                    v_text[i * 2 - 1] = {space_char:rep(space - 1), "Conceal"}
+                    v_text[i * 2] = {c, "Whitespace"}
+                end
+
+                vim.schedule_wrap(
+                    function()
+                        xpcall(
+                            function()
+                                vim.api.nvim_buf_set_virtual_text(
+                                    buf,
+                                    vim.g.indent_blankline_namespace,
+                                    i - 1,
+                                    v_text,
+                                    vim.empty_dict()
+                                )
+                            end,
+                            error_handler
                         )
-                    end, error_handler)
-                end
-            )()
-            return async:close()
-        end)
+                    end
+                )()
+                return async:close()
+            end
+        )
 
         async:send()
     end
