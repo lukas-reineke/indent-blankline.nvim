d3baedb8013d569278f7baa163f14f09c5de52a1
Lukas Reineke <lukas.reineke@protonmail.com>
2021-03-22 23:32:27 +0900

    removed debugging

--- lua/indent_blankline/init.lua
+++ lua/indent_blankline/init.lua
@@ -142,7 +142,6 @@ local refresh = function()
                                 return
                             end
                             indent = indent / space
-                            P(indent)
                             if offset + i == context_start then
                                 context_indent = (indent or 0) + 1
                             end
