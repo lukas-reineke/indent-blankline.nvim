4b1a9b6f22cc184770229d9a6ccb1b76dd7d2830
ek234 <71319388+ek234@users.noreply.github.com>
2021-06-08 22:09:30 +0530

    uncommented code after testing

--- lua/indent_blankline/init.lua
+++ lua/indent_blankline/init.lua
@@ -177,32 +177,32 @@ local refresh = function()
                 end
         end
         
- --       if ((blankline and trail_indent) or extra) and (first_indent or #virtual_text > 0) then
- --           local index = math.ceil(#virtual_text / 2) + 1
- --           table.insert(
- --               virtual_text,
- --               {
- --                   utils._if(
- --                       #char_list > 0,
- --                       utils.get_from_list(char_list, index - utils._if(not first_indent, 1, 0)),
- --                       char
- --                   ),
- --                   utils._if(
- --                       context_active and context_indent == index,
- --                       utils._if(
- --                           #context_highlight_list > 0,
- --                           utils.get_from_list(context_highlight_list, index),
- --                           context_highlight
- --                       ),
- --                       utils._if(
- --                           #char_highlight_list > 0,
- --                           utils.get_from_list(char_highlight_list, index),
- --                           char_highlight
- --                       )
- --                   )
- --               }
- --           )
- --       end
+        if ((blankline and trail_indent) or extra) and (first_indent or #virtual_text > 0) then
+            local index = math.ceil(#virtual_text / 2) + 1
+            table.insert(
+                virtual_text,
+                {
+                    utils._if(
+                        #char_list > 0,
+                        utils.get_from_list(char_list, index - utils._if(not first_indent, 1, 0)),
+                        char
+                    ),
+                    utils._if(
+                        context_active and context_indent == index,
+                        utils._if(
+                            #context_highlight_list > 0,
+                            utils.get_from_list(context_highlight_list, index),
+                            context_highlight
+                        ),
+                        utils._if(
+                            #char_highlight_list > 0,
+                            utils.get_from_list(char_highlight_list, index),
+                            char_highlight
+                        )
+                    )
+                }
+            )
+        end
 
         return virtual_text
     end
