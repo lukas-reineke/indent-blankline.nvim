0c4ef8cbb860c7b33a64f675492148806d88b340
ek234 <71319388+ek234@users.noreply.github.com>
2021-06-09 15:04:27 +0530

    added support for first_indent

--- lua/indent_blankline/init.lua
+++ lua/indent_blankline/init.lua
@@ -139,7 +139,7 @@ local refresh = function()
 
     local get_virtual_text = function(indent, extra, blankline, context_active, context_indent)
         local virtual_text = {}
-        for i = utils.if(first_indent, 1, 2), math.min(math.max(indent, 0), max_indent_level) do
+        for i = utils._if(first_indent, 1, 2), math.min(math.max(indent, 0), max_indent_level) do
             local context = context_active and context_indent == i
             -- first char in indent:
                 if( i == 1 and blankline and end_of_line and #end_of_line_char > 0 ) then
