bbebac6fdc3bc3c8ad87d6cbbc210118b65d8174
Lukas Reineke <lukas.reineke@protonmail.com>
2023-09-28 17:19:50 +0900

    fix: update deprecation message to point to the github wiki

--- lua/indent_blankline.lua
+++ lua/indent_blankline.lua
@@ -1,7 +1,7 @@
 return {
     setup = function()
         vim.notify_once(
-            "You are trying to call the setup function of indent-blankline version 2, but you have version 3 installed.\nTake a look at the readme for instructions on how to migrate, or revert back to version 2.",
+            "You are trying to call the setup function of indent-blankline version 2, but you have version 3 installed.\nTake a look at the GitHub wiki for instructions on how to migrate, or revert back to version 2.",
             vim.log.levels.ERROR
         )
     end,
