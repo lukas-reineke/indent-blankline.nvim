d9f19fbd5a82bca2abcf3f16192a332cd8daad55
Daniel Kongsgaard <daniel@Daniels-MBP.home>
2023-10-10 00:25:53 +0200

    Fix small mistake in last "fix"

--- lua/ibl/init.lua
+++ lua/ibl/init.lua
@@ -429,8 +429,8 @@ M.refresh = function(bufnr)
                 -- row > cursor_row
                 -- if cur_indent_stack_size is 1, we should stop when we get to a line with no more whitespace in the whitespace_tbl
                 if
-                    (cursor_row_stack_size >= 0 and cursor_row_stack_size > cur_indent_stack_size and cur_indent_stack_size ~= 1)
-                    or (cursor_row_stack_size >= 0 and cur_indent_stack_size == 1 and #whitespace_tbl == 0)
+                    (cursor_row_stack_size >= 0 and cursor_row_stack_size > cur_indent_stack_size and (not blankline))
+                    or (cursor_row_stack_size >= 0 and cur_indent_stack_size >= 1 and #whitespace_tbl == 0)
                 then
                     current_indent_row_end = row - 1
                     cursor_row_stack_size = -1
